6e26f5a0f7657163bc380712666cb15b8596fb5f
Marina Delneri <mgdelneri@gmail.com>
2021-03-13 10:00:44 -0600

    Wrote pseudo-code in JavaScript file

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// TODO: Show the day at the top of the page
+// TODO: Use Bootstrap to make the calendar slots
+// TODO: Put time on each of the slots
+// TODO: Make it so you can enter input for each of the time slots
+// TODO: Have a "save" button at the end of each of the time slots
+    // TODO: Save input in local storage, so it stays on page when you refresh page
+// TODO: Make slots change color when past, preset, future

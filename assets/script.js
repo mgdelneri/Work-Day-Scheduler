e339eee6cf31f0d51e548f098a6ff9534f0bc6bf
Marina Delneri <mgdelneri@gmail.com>
2021-03-15 08:29:46 -0500

    Added save buttons to input fields

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,11 @@
+// Displays today's date in the header
 var today = moment();
-$("#currentDay").text(today.format("dddd") + ", " + today.format("MMMM Do, YYYY"));
-
-
-
-
-
-
+$("#currentDay").text(
+  today.format("dddd") + ", " + today.format("MMMM Do, YYYY")
+);
 
 
 // TODO: Make it so you can enter input for each of the time slots
 // TODO: Have a "save" button at the end of each of the time slots
-    // TODO: Save input in local storage, so it stays on page when you refresh page
+// TODO: Save input in local storage, so it stays on page when you refresh page
 // TODO: Make slots change color when past, preset, future

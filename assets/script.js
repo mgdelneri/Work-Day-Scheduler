d7a607d838117de79cfee8f8042e1658dc32b86a
Marina Delneri <mgdelneri@gmail.com>
2021-03-14 23:01:20 -0500

    Edited time blocks to include input field and continued to style page

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,8 @@ $("#currentDay").text(today.format("dddd") + ", " + today.format("MMMM Do, YYYY"
 
 
 
-// TODO: Show the day at the top of the page
-// TODO: Use Bootstrap to make the calendar slots
-// TODO: Put time on each of the slots
+
+
 // TODO: Make it so you can enter input for each of the time slots
 // TODO: Have a "save" button at the end of each of the time slots
     // TODO: Save input in local storage, so it stays on page when you refresh page

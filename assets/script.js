454855c49d9b45feff5173cb175aa30391ddc3ee
Marina Delneri <mgdelneri@gmail.com>
2021-03-15 11:49:03 -0500

    In JavaScript, experimented with button functions and local storage; In HTML, set unique ids for all inputs and butons

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,30 @@
-// Displays today's date in the header
+// Set variables
 var today = moment();
+var btn9am = document.querySelector("#btn9am");
+var input9am = document.querySelector("#stuff9am");
+
+// Displays today's date in the header
 $("#currentDay").text(
   today.format("dddd") + ", " + today.format("MMMM Do, YYYY")
 );
 
+// Get input items from local storage for each time slot when page is opened/refreshed
+renderLast9amInput();
 
-// TODO: Make it so you can enter input for each of the time slots
-// TODO: Have a "save" button at the end of each of the time slots
+function renderLast9amInput() {
+    var last9am = localStorage.getItem("data9am");
+    
+    if (!input9am) {
+        return;
+    };
+    input9am.value = last9am;
+}
+
+// Add event listeners to all buttons individually
+btn9am.addEventListener("click", function(event) {
+  event.preventDefault();
+  localStorage.setItem("data9am", input9am.value);
+});
+
 // TODO: Save input in local storage, so it stays on page when you refresh page
 // TODO: Make slots change color when past, preset, future
